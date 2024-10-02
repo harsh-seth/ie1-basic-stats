605376b15ef6fd0ed915e06334d97bb39b444085
TomNgo03 <ngoquangtung281203@gmail.com>
2024-10-02 12:12:32 -0400

    feat(min-max): Added findMinMax function

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,5 +1,15 @@
 public class MinMaxCalculation {
     public static void main(String[] args) {
         System.err.println("Hello World, from Team 48!");
+
+        int[] newArr = new int[]{1,2,3,4,5,6,7,8,9};
+        int[] ans = findMinMax(newArr);
+        System.out.println("Min value:" + ans[0] + "Max value:" + ans[1]);
+    }
+
+    public static int[] findMinMax(int[] nums) {
+        
+
+        return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE};
     }
 }
