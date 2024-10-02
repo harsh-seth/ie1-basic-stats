eac58f35eef2979a97674e41dbb299e8dc2a93fb
Harsh Seth <hseth@umass.edu>
2024-10-02 12:15:26 -0400

    feat(min-max): standardized outputs

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -4,7 +4,7 @@ public class MinMaxCalculation {
 
         int[] newArr = new int[]{1,2,3,4,5,6,7,8,9};
         int[] ans = findMinMax(newArr);
-        System.out.println("Min value:" + ans[0] + "Max value:" + ans[1]);
+        System.out.println("Min value: " + ans[0] + "\nMax value: " + ans[1]);
     }
 
     public static int[] findMinMax(int[] nums) {
